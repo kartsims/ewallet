c56c47f0dfd7dbf6b5724d335dfd996bd5d52c52
Kridsada Thanabulpong <sirn@omise.co>
2018-11-20 01:50:51 +0700

    Bump version to 1.1.0-pre.

--- apps/local_ledger/mix.exs
+++ apps/local_ledger/mix.exs
@@ -4,7 +4,7 @@ defmodule LocalLedger.Mixfile do
   def project do
     [
       app: :local_ledger,
-      version: "0.1.0-beta",
+      version: "1.1.0-pre",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
